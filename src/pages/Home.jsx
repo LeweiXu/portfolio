21e95be1cbfe799617be3b5750dbc1de985a487e
LeweiXu <leweixu@gmail.com>
2026-03-19 21:20:38 +0800

    minor change to interests

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -91,7 +91,7 @@ export default function Home() {
             <li>Gym - shoulders are fine but somehow have a right tricep injury</li>
             <li>Anime - manga, light novels, you name it, let's not dwell too much on this one</li>
             <li>Snooker - probably messing up my back over time but whatever</li>
-            <li>Eating - almost hit 90kg at one point as an Asian of average height</li>
+            <li>Eating - it's in this list cause I almost hit 90kg in weight at one point</li>
           </ul>
         </div>
       </div>
